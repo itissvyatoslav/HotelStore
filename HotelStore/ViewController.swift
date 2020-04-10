af40a048a23520b40911c3ce0a6ac8723647b3b7
Shevchenko Svyatoslav <itissvyatoslav@mail.ru>
2020-04-10 20:09:14 +0700

    Initial Commit

---
+++ HotelStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotelStore
+//
+//  Created by Svyatoslav Vladimirovich on 10.04.2020.
+//  Copyright © 2020 Svyatoslav Vladimirovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
